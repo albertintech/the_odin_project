80a021614b0271f83f0a97d046cc590fedf500b2
Albert Ramos <albertramos.dev@gmail.com>
2021-11-10 09:25:28 -0700

    Add scratch file

---
+++ basic_ruby/5_loops/scratch.rb
@@ -0,0 +1,16 @@
+# the "loop" loop
+
+# i = 0
+# loop do
+#   puts "i is #{i}"
+#   i += 1
+#   break if i == 10 # intention of code last :(
+# end
+
+# while loop
+
+i = 0
+while i < 10 do # intention of code first :)
+  puts "i is #{i}"
+  i += 1
+end
