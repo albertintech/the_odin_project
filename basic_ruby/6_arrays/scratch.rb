b53871d354e28f4e7616197fb3e9eb241ac4ca53
Albert Ramos <albertramos.dev@gmail.com>
2021-11-10 16:59:24 -0700

    Update file

--- basic_ruby/6_arrays/scratch.rb
+++ basic_ruby/6_arrays/scratch.rb
@@ -1,2 +1,33 @@
-arr = [1, 3, 5, 7, 9, 11]
-number = 3
+def find_longest(string)
+  spl = string.split(" ")
+  longest = 0
+  i=0
+    while (i < spl.length) do
+      if (spl[i].length > longest)
+        longest = spl[i].length
+      end
+      i += 1
+    end
+  return longest
+end
+
+# def find_longest(string)
+#   spl = string.split(" ")
+#   longest = 0
+#   i=0
+#   while (i < spl.length) do
+#     if (spl[i].length > longest)
+#       longest = spl[i].length
+#     end
+#     i += 1
+#   end
+#   return longest
+# end
+
+
+p find_longest("two massive dogs")
+
+# Most Ruby solution
+# def find_longest(string)
+#   string.split.map(&:length).max
+# end
