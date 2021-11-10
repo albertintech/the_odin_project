0bf943f306d8a487a0fba1f0fd28a727398a8271
Albert Ramos <albertramos.dev@gmail.com>
2021-11-09 17:17:27 -0700

    Add scratch file

---
+++ basic_ruby/4_conditional_logic/scratch_file.rb
@@ -0,0 +1,10 @@
+attack_by_land = nil
+attack_by_sea = nil
+
+if attack_by_land == true
+  puts "Release Kong!"
+elsif attack_by_sea == true
+  puts "Release the Kraken!"
+else
+  puts "Release Mothra!"
+end
