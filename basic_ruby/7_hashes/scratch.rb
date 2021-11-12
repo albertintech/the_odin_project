970d62ed54b456d65bf10376d2c0ba7f77870b90
Albert Ramos <albertramos.dev@gmail.com>
2021-11-12 08:10:16 -0700

    Update file

--- basic_ruby/7_hashes/scratch.rb
+++ basic_ruby/7_hashes/scratch.rb
@@ -1,5 +1,15 @@
-person = {name: 'Bob', height: '6 ft', weight: '160 lbs', hair: 'brown'}
+# person = {name: 'Bob', height: '6 ft', weight: '160 lbs', hair: 'brown'}
+#
+# person.each do |k,v|
+#   puts "Bob's #{k} is #{v}"
+# end
 
-person.each do |k,v|
-  puts "Bob's #{k} is #{v}"
-end
+family = {  uncles: ["bob", "joe", "steve"],
+            sisters: ["jane", "jill", "beth"],
+            brothers: ["frank","rob","david"],
+            aunts: ["mary","sally","susan"]
+          }
+
+new_arr = family.select {|k, v| k == :sisters || k == :brothers }
+
+p new_arr.values.flatten
