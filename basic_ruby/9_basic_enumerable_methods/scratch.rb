844fe431248a2b29e0f5151467b5e30a18cd22bf
Albert Ramos <albertramos.dev@gmail.com>
2021-11-14 07:10:39 -0700

    Update file

--- basic_ruby/9_basic_enumerable_methods/scratch.rb
+++ basic_ruby/9_basic_enumerable_methods/scratch.rb
@@ -1,1 +1,7 @@
 # Scratch file for TOP Basic Ruby Part 9: Basic Enumerable Methods
+array = %w(a b c d e)
+p array
+
+indexed_array = array.map.with_index { |ch, index| [ch, index]}
+
+p indexed_array
