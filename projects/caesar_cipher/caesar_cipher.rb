f8332d2dda2f12c116f14589b47d5a49e6cdfca3
Albert Ramos <albertramos.dev@gmail.com>
2021-11-23 19:08:22 -0700

    Add solution

--- projects/caesar_cipher/caesar_cipher.rb
+++ projects/caesar_cipher/caesar_cipher.rb
@@ -1,18 +1,12 @@
 # Implement a caesar cipher that takes in a single word string and the shift factor and then outputs the modified string:
 #
-# > caesar_cipher("What a string!", 5)
-# => "Bmfy f xywnsl!"
-#
-# Quick Tips:
-#
-# You will need to remember how to convert a string into a number.
-# Don’t forget to wrap from z to a.
-# Don’t forget to keep the same case.
+# > caesar_cipher("nope", 5)
+# => "opqf"
+
 
 def caesar_cipher(str, increment)
   alphabet = "abcdefghijklmnopqrstuvwxyz"
   new_str = ""
-
   str.each_char do |char|
     old_index = alphabet.index(char)
     new_index = old_index + increment
@@ -22,6 +16,6 @@ def caesar_cipher(str, increment)
   return new_str
 end
 
-puts caesar_cipher("apple", 1)    #=> "bqqmf"
+puts caesar_cipher("nope", 1)    #=> "bqqmf"
 puts caesar_cipher("bootcamp", 2) #=> "dqqvecor"
 puts caesar_cipher("zebra", 4)    #=> "difve"
