613a224edacedcd4fb3e7cbf19c07f5d12e7ae3d
Albert Ramos <albertramos.dev@gmail.com>
2021-11-23 19:02:19 -0700

    Add solution

--- projects/caesar_cipher/caesar_cipher.rb
+++ projects/caesar_cipher/caesar_cipher.rb
@@ -1,4 +1,4 @@
-# Implement a caesar cipher that takes in a string and the shift factor and then outputs the modified string:
+# Implement a caesar cipher that takes in a single word string and the shift factor and then outputs the modified string:
 #
 # > caesar_cipher("What a string!", 5)
 # => "Bmfy f xywnsl!"
@@ -9,37 +9,19 @@
 # Don’t forget to wrap from z to a.
 # Don’t forget to keep the same case.
 
-# -Input: String, integer
-# -Output: String
-#
-# Accept string and integer
-# -String will be transformed
-# -Integer will be shift factor
+def caesar_cipher(str, increment)
+  alphabet = "abcdefghijklmnopqrstuvwxyz"
+  new_str = ""
 
-
-
-alphabet = %w(a b c d e f g h i j k l m n o p q r s t u v w x y z a b c)
-
-p alphabet
-
-puts "Give me a string to encrypt with the Caesar Cipher: "
-user_string = gets.chomp.downcase
-
-arr = user_string.split('')
-p arr
-arr.each_with_index do |letter, index|
-  puts "The letter is '#{letter} and the index is #{index}.'"
-end
-
-
-puts "Give me a right shift factor between 1 and 3: "
-shift_factor = gets.chomp.to_i
-
-def caesar_cipher(string, shift)
-  arr = user_string.split('')
-  arr.map.each_with_index {|letter, index| index + shift }
-
+  str.each_char do |char|
+    old_index = alphabet.index(char)
+    new_index = old_index + increment
+    new_char = alphabet[new_index % 26]
+    new_str += new_char
   end
+  return new_str
 end
 
-caesar_cipher(user_string,shift_factor)
+puts caesar_cipher("apple", 1)    #=> "bqqmf"
+puts caesar_cipher("bootcamp", 2) #=> "dqqvecor"
+puts caesar_cipher("zebra", 4)    #=> "difve"
